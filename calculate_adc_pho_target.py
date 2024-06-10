b9eaf8a02d75706ab2983c18d6585c0852fbb06c
Anna Elise Wong <awong@tripleringtech.com>
2024-06-10 10:56:32 -0700

    Update extract adc_pho_test_results format. Create script for calculating the target adc/voltage values for adc pho test based on provided variables. Outputs a file in a format that allows for directly copying and pasting into the results template

---
+++ calculate_adc_pho_target.py
@@ -0,0 +1,114 @@
+import csv
+
+# Variables!!!!
+ANALOG_GAIN         = 2.375
+ADC_REF             = 2.048
+POWER_SUPPLY_CH1    = 0.018         # V
+POWER_SUPPLY_CH2    = 0.09          # V
+POWER_SUPPLY_CH3    = 0.17          # V
+DAC_GAIN_MAX        = 1
+DAC_GAIN_80_PERCENT = 0.8
+DAC_GAIN_40_PERCENT = 0.4
+ADC_REF_MAX         = 7864320
+INT_RC              = 0.000027336
+INT_TIME            = 0.0001        # s
+
+# Create output file
+outfilename = "ADCPhoTargetValues.csv"
+
+print("Output file: %s" % outfilename)
+
+name = []
+targetV = []
+targetADCCount = []
+
+with open(outfilename,'wt', newline='') as myfile:
+    wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
+
+#--------------------------------------------------------------------------------------#
+
+    # Test 1
+
+    ch0_targetV = POWER_SUPPLY_CH1 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch3_targetV = POWER_SUPPLY_CH2 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch7_targetV = POWER_SUPPLY_CH3 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+
+    ch0_targetADCCount = ch0_targetV * ADC_REF_MAX / ADC_REF
+    ch3_targetADCCount = ch3_targetV * ADC_REF_MAX / ADC_REF
+    ch7_targetADCCount = ch7_targetV * ADC_REF_MAX / ADC_REF
+
+    name.append(["Test #1"])
+    targetV.append([ch0_targetV, "", ch3_targetV, "", ch7_targetV])
+    targetADCCount.append([round(ch0_targetADCCount), "", round(ch3_targetADCCount), "", round(ch7_targetADCCount)])
+
+#--------------------------------------------------------------------------------------#
+
+    # Test 2
+
+    ch0_targetV = POWER_SUPPLY_CH1 * ANALOG_GAIN * DAC_GAIN_80_PERCENT * (1 + (INT_TIME / INT_RC))
+    ch3_targetV = POWER_SUPPLY_CH2 * ANALOG_GAIN * DAC_GAIN_80_PERCENT * (1 + (INT_TIME / INT_RC))
+    ch7_targetV = POWER_SUPPLY_CH3 * ANALOG_GAIN * DAC_GAIN_80_PERCENT * (1 + (INT_TIME / INT_RC))
+
+    ch0_targetADCCount = ch0_targetV * ADC_REF_MAX / ADC_REF
+    ch3_targetADCCount = ch3_targetV * ADC_REF_MAX / ADC_REF
+    ch7_targetADCCount = ch7_targetV * ADC_REF_MAX / ADC_REF
+
+    name.append(["Test #2"])
+    targetV.append([ch0_targetV, "", ch3_targetV, "", ch7_targetV])
+    targetADCCount.append([round(ch0_targetADCCount), "", round(ch3_targetADCCount), "", round(ch7_targetADCCount)])
+
+#--------------------------------------------------------------------------------------#
+
+    # Test 3
+
+    ch0_targetV = POWER_SUPPLY_CH1 * ANALOG_GAIN * DAC_GAIN_40_PERCENT * (1 + (INT_TIME / INT_RC))
+    ch3_targetV = POWER_SUPPLY_CH2 * ANALOG_GAIN * DAC_GAIN_40_PERCENT * (1 + (INT_TIME / INT_RC))
+    ch7_targetV = POWER_SUPPLY_CH3 * ANALOG_GAIN * DAC_GAIN_40_PERCENT * (1 + (INT_TIME / INT_RC))
+
+    ch0_targetADCCount = ch0_targetV * ADC_REF_MAX / ADC_REF
+    ch3_targetADCCount = ch3_targetV * ADC_REF_MAX / ADC_REF
+    ch7_targetADCCount = ch7_targetV * ADC_REF_MAX / ADC_REF
+
+    name.append(["Test #3"])
+    targetV.append([ch0_targetV, "", ch3_targetV, "", ch7_targetV])
+    targetADCCount.append([round(ch0_targetADCCount), "", round(ch3_targetADCCount), "", round(ch7_targetADCCount)])
+
+#--------------------------------------------------------------------------------------#
+
+    # Test 4
+
+    ch0_targetV = POWER_SUPPLY_CH2 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch3_targetV = POWER_SUPPLY_CH3 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch7_targetV = POWER_SUPPLY_CH1 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+
+    ch0_targetADCCount = ch0_targetV * ADC_REF_MAX / ADC_REF
+    ch3_targetADCCount = ch3_targetV * ADC_REF_MAX / ADC_REF
+    ch7_targetADCCount = ch7_targetV * ADC_REF_MAX / ADC_REF
+
+    name.append(["Test #4"])
+    targetV.append([ch0_targetV, "", ch3_targetV, "", ch7_targetV])
+    targetADCCount.append([round(ch0_targetADCCount), "", round(ch3_targetADCCount), "", round(ch7_targetADCCount)])
+
+#--------------------------------------------------------------------------------------#
+
+    # Test 5
+    
+    ch0_targetV = POWER_SUPPLY_CH3 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch3_targetV = POWER_SUPPLY_CH1 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+    ch7_targetV = POWER_SUPPLY_CH2 * ANALOG_GAIN * DAC_GAIN_MAX * (1 + (INT_TIME / INT_RC))
+
+    ch0_targetADCCount = ch0_targetV * ADC_REF_MAX / ADC_REF
+    ch3_targetADCCount = ch3_targetV * ADC_REF_MAX / ADC_REF
+    ch7_targetADCCount = ch7_targetV * ADC_REF_MAX / ADC_REF
+
+    name.append(["Test #5"])
+    targetV.append([ch0_targetV, "", ch3_targetV, "", ch7_targetV])
+    targetADCCount.append([round(ch0_targetADCCount), "", round(ch3_targetADCCount), "", round(ch7_targetADCCount)])
+
+#--------------------------------------------------------------------------------------#
+
+    for n, v, ct in zip(name, targetV, targetADCCount):
+            wrtr.writerow(n)
+            wrtr.writerow(v)
+            wrtr.writerow(ct)
+            wrtr.writerow("\n")
--- extract_adc_pho_test_results.py
+++ extract_adc_pho_test_results.py
@@ -139,8 +139,9 @@ def convert32BitTo16Bit(val):
 #--------------------------------------------------------------------------------------------#
 
 def usage():
-    print("extract_beak_offset_stats.py -i <rotor name> [-o]")
+    print("extract_beak_offset_stats.py -i <test name> [-b] [-o]")
     print(" -i <test name> Full prefix name of rotor")
+    print(" -b Flag to indicate test was a baseline")
     print(" -o Flag to indicate output file should be created")
     sys.exit(0)
 
@@ -150,13 +151,14 @@ if argc < 2:
 
 # Parse the command line options
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "i:o")
+    opts, args = getopt.getopt(sys.argv[1:], "i:ob")
 except getopt.error:
     usage()
     sys.exit(2)
     
 # Process options
 test_name = ""
+BASELINE_TEST = False
 CREATE_OUTPUT_FILE = False
      
 for o, a in opts:
@@ -164,6 +166,8 @@ for o, a in opts:
         test_name = a
     elif o == "-o":
         CREATE_OUTPUT_FILE = True
+    elif o == "-b":
+        BASELINE_TEST = True
 
 if test_name == "":
     usage()
@@ -173,11 +177,7 @@ test_path = os.path.abspath(test_name)
 base_name = os.path.basename(test_name)
 
 outfilename = test_name + "_ResultsOut.csv"
-# try:
-#     fileOut = open(outfilename, 'wt')
-# except:
-#     print("Could not open %s" % outfilename)
-#     sys.exit(0)
+
 fileOut = open(outfilename, 'wt')
 writer = csv.writer(fileOut, delimiter = ',', quotechar='"')
 
@@ -234,46 +234,140 @@ ch0_proc_data_16bit = []
 ch3_proc_data_16bit = []
 ch7_proc_data_16bit = []
 
+if BASELINE_TEST:
+    ch1_raw_data_32bit = []
+    ch2_raw_data_32bit = []
+    ch4_raw_data_32bit = []
+    ch5_raw_data_32bit = []
+    ch6_raw_data_32bit = []
+    ch8_raw_data_32bit = []
+    ch9_raw_data_32bit = []
+
+    ch1_proc_data_16bit = []
+    ch2_proc_data_16bit = []
+    ch4_proc_data_16bit = []
+    ch5_proc_data_16bit = []
+    ch6_proc_data_16bit = []
+    ch8_proc_data_16bit = []
+    ch9_proc_data_16bit = []
+
 # 1. Make sure the PuTTy and MsgOut data matches
     # mo = msgout, ec=engconsole
 for vals_mo, vals_ec in zip(pho_data_16bit, converted_signed_data):
     if vals_mo[0] != vals_ec[0]:
-        print("CH0 [Error]: Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[0], vals_ec[0]))
+        print("Error: [CH0] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[0], vals_ec[0]))
         sys.exit(1)
     else:
         ch0_proc_data_16bit.append(vals_mo[0])
     if vals_mo[3] != vals_ec[3]:
-        print("CH3 [Error]: Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[3], vals_ec[3]))
+        print("Error: [CH3] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[3], vals_ec[3]))
         sys.exit(1)
     else:
         ch3_proc_data_16bit.append(vals_mo[3])
     if vals_mo[7] != vals_ec[7]:
-        print("CH7 [Error]: Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[7], vals_ec[7]))
+        print("Error: [CH7] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[7], vals_ec[7]))
         sys.exit(1)
     else:
         ch7_proc_data_16bit.append(vals_mo[7])
+    
+    if BASELINE_TEST:
+        if vals_mo[1] != vals_ec[1]:
+            print("Error: [CH1] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[1], vals_ec[1]))
+            sys.exit(1)
+        else:
+            ch1_proc_data_16bit.append(vals_mo[1])
+        if vals_mo[2] != vals_ec[2]:
+            print("Error: [CH2] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[2], vals_ec[2]))
+            sys.exit(1)
+        else:
+            ch2_proc_data_16bit.append(vals_mo[2])
+        if vals_mo[4] != vals_ec[4]:
+            print("Error: [CH4] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[4], vals_ec[4]))
+            sys.exit(1)
+        else:
+            ch4_proc_data_16bit.append(vals_mo[4])
+        if vals_mo[5] != vals_ec[5]:
+            print("Error: [CH5] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[5], vals_ec[5]))
+            sys.exit(1)
+        else:
+            ch5_proc_data_16bit.append(vals_mo[5])
+        if vals_mo[6] != vals_ec[6]:
+            print("Error: [CH6] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[6], vals_ec[6]))
+            sys.exit(1)
+        else:
+            ch6_proc_data_16bit.append(vals_mo[6])
+        if vals_mo[8] != vals_ec[8]:
+            print("Error: [CH8] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[8], vals_ec[8]))
+            sys.exit(1)
+        else:
+            ch8_proc_data_16bit.append(vals_mo[8])
+        if vals_mo[9] != vals_ec[9]:
+            print("Error: [CH9] Values don't match: MsgOut val=%d, EngConsole val=%d" % (vals_mo[9], vals_ec[9]))
+            sys.exit(1)
+        else:
+            ch9_proc_data_16bit.append(vals_mo[9])
+        
 
 # 2. Extract the raw values
 for vals in raw_pho_data:
     ch0_raw_data_32bit.append(vals[0])
     ch3_raw_data_32bit.append(vals[3])
     ch7_raw_data_32bit.append(vals[7])
+
+    if BASELINE_TEST:
+        ch1_raw_data_32bit.append(vals[1])
+        ch2_raw_data_32bit.append(vals[2])
+        ch4_raw_data_32bit.append(vals[4])
+        ch5_raw_data_32bit.append(vals[5])
+        ch6_raw_data_32bit.append(vals[6])
+        ch8_raw_data_32bit.append(vals[8])
+        ch9_raw_data_32bit.append(vals[9])
     
 # 3. Verify the actual 16 bit numbers match the converted raw values
 for raw_vals, proc_vals in zip(ch0_raw_data_32bit, ch0_proc_data_16bit):
     conv_raw_vals = convert32BitTo16Bit(raw_vals)
     if conv_raw_vals != proc_vals:
-        print("CH0 [Error]: Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+        print("Error: [CH0] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
 
 for raw_vals, proc_vals in zip(ch3_raw_data_32bit, ch3_proc_data_16bit):
     conv_raw_vals = convert32BitTo16Bit(raw_vals)
     if conv_raw_vals != proc_vals:
-        print("CH3 [Error]: Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+        print("Error: [CH3] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
 
 for raw_vals, proc_vals in zip(ch7_raw_data_32bit, ch7_proc_data_16bit):
     conv_raw_vals = convert32BitTo16Bit(raw_vals)
     if conv_raw_vals != proc_vals:
-        print("CH7 [Error]: Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+        print("Error: [CH7] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+
+if BASELINE_TEST:
+    for raw_vals, proc_vals in zip(ch1_raw_data_32bit, ch1_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch2_raw_data_32bit, ch2_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch4_raw_data_32bit, ch4_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch5_raw_data_32bit, ch5_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch6_raw_data_32bit, ch6_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch8_raw_data_32bit, ch8_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
+    for raw_vals, proc_vals in zip(ch9_raw_data_32bit, ch9_proc_data_16bit):
+        conv_raw_vals = convert32BitTo16Bit(raw_vals)
+        if conv_raw_vals != proc_vals:
+            print("Error: [CH1] Expected value (%d) does not match actual value (%d)" % (conv_raw_vals, proc_vals))
 
 # 4. Find the statistics for the values
 ch0_raw_avg = np.mean(ch0_raw_data_32bit)
@@ -294,6 +388,91 @@ ch7_raw_std = np.std(ch7_raw_data_32bit)
 ch7_proc_avg = np.mean(ch7_proc_data_16bit)
 ch7_proc_std = np.std(ch7_proc_data_16bit)
 
+if BASELINE_TEST:
+    ch1_raw_avg = np.mean(ch1_raw_data_32bit)
+    ch1_raw_std = np.std(ch1_raw_data_32bit)
+
+    ch1_proc_avg = np.mean(ch1_proc_data_16bit)
+    ch1_proc_std = np.std(ch1_proc_data_16bit)
+
+    ch2_raw_avg = np.mean(ch2_raw_data_32bit)
+    ch2_raw_std = np.std(ch2_raw_data_32bit)
+
+    ch2_proc_avg = np.mean(ch2_proc_data_16bit)
+    ch2_proc_std = np.std(ch2_proc_data_16bit)
+
+    ch4_raw_avg = np.mean(ch4_raw_data_32bit)
+    ch4_raw_std = np.std(ch4_raw_data_32bit)
+
+    ch4_proc_avg = np.mean(ch4_proc_data_16bit)
+    ch4_proc_std = np.std(ch4_proc_data_16bit)
+
+    ch5_raw_avg = np.mean(ch5_raw_data_32bit)
+    ch5_raw_std = np.std(ch5_raw_data_32bit)
+
+    ch5_proc_avg = np.mean(ch5_proc_data_16bit)
+    ch5_proc_std = np.std(ch5_proc_data_16bit)
+
+    ch6_raw_avg = np.mean(ch6_raw_data_32bit)
+    ch6_raw_std = np.std(ch6_raw_data_32bit)
+
+    ch6_proc_avg = np.mean(ch6_proc_data_16bit)
+    ch6_proc_std = np.std(ch6_proc_data_16bit)
+
+    ch8_raw_avg = np.mean(ch8_raw_data_32bit)
+    ch8_raw_std = np.std(ch8_raw_data_32bit)
+
+    ch8_proc_avg = np.mean(ch8_proc_data_16bit)
+    ch8_proc_std = np.std(ch8_proc_data_16bit)
+
+    ch9_raw_avg = np.mean(ch9_raw_data_32bit)
+    ch9_raw_std = np.std(ch9_raw_data_32bit)
+
+    ch9_proc_avg = np.mean(ch9_proc_data_16bit)
+    ch9_proc_std = np.std(ch9_proc_data_16bit)
+
+    print("\n     CH0 32-bit  CH0 16-bit  CH1 32-bit  CH1 16-bit  CH2 32-bit  CH2 16-bit  CH3 32-bit  CH3 16-bit  CH4 32-bit  CH4 16-bit  CH5 32-bit  CH5 16-bit  CH6 32-bit  CH6 16-bit  CH7 32-bit  CH7 16-bit  CH8 32-bit  CH8 16-bit  CH9 32-bit  CH9 16-bit")
+    print("     ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------")
+    for i in range(10):
+        print(" (%d) %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d" % \
+              (i, ch0_raw_data_32bit[i], ch0_proc_data_16bit[i], ch1_raw_data_32bit[i], ch1_proc_data_16bit[i], ch2_raw_data_32bit[i], ch2_proc_data_16bit[i], \
+               ch3_raw_data_32bit[i], ch3_proc_data_16bit[i], ch4_raw_data_32bit[i], ch4_proc_data_16bit[i], ch5_raw_data_32bit[i], ch5_proc_data_16bit[i], \
+                ch6_raw_data_32bit[i], ch6_proc_data_16bit[i], ch7_raw_data_32bit[i], ch7_proc_data_16bit[i], ch8_raw_data_32bit[i], ch8_proc_data_16bit[i], ch9_raw_data_32bit[i], ch9_proc_data_16bit[i]))
+    print("     ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------  ----------")
+    print("AVG: %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d" % \
+          (ch0_raw_avg, ch0_proc_avg, ch1_raw_avg, ch1_proc_avg, ch2_raw_avg, ch2_proc_avg, ch3_raw_avg, ch3_proc_avg, ch4_raw_avg, ch4_proc_avg,\
+           ch5_raw_avg, ch5_proc_avg, ch6_raw_avg, ch6_proc_avg, ch7_raw_avg, ch7_proc_avg, ch8_raw_avg, ch8_proc_avg, ch9_raw_avg, ch9_proc_avg))
+    print("STD: %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d  %10d" % \
+          (ch0_raw_std, ch0_proc_std, ch1_raw_std, ch1_proc_std, ch2_raw_std, ch2_proc_std, ch3_raw_std, ch3_proc_std, ch4_raw_std, ch4_proc_std,\
+           ch5_raw_std, ch5_proc_std, ch6_raw_std, ch6_proc_std, ch7_raw_std, ch7_proc_std, ch8_raw_std, ch8_proc_std, ch9_raw_std, ch9_proc_std))
+    
+    with open(outfilename,'wt', newline='') as myfile:
+        wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
+        rows = []
+        rows.append(["","CH0 32-bit", "CH0 16-bit","CH1 32-bit", "CH1 16-bit","CH2 32-bit", "CH2 16-bit", "CH3 32-bit", "CH3 16-bit", "CH4 32-bit", "CH4 16-bit",\
+                     "CH5 32-bit", "CH5 16-bit", "CH6 32-bit", "CH6 16-bit", "CH7 32-bit", "CH7 16-bit", "CH8 32-bit", "CH8 16-bit", "CH9 32-bit", "CH9 16-bit"])
+        for i in range(10):
+            rows.append([i, ch0_raw_data_32bit[i], ch0_proc_data_16bit[i], \
+                         ch1_raw_data_32bit[i], ch1_proc_data_16bit[i], \
+                         ch2_raw_data_32bit[i], ch2_proc_data_16bit[i], \
+                         ch3_raw_data_32bit[i], ch3_proc_data_16bit[i], \
+                         ch4_raw_data_32bit[i], ch4_proc_data_16bit[i], \
+                         ch5_raw_data_32bit[i], ch5_proc_data_16bit[i], \
+                         ch6_raw_data_32bit[i], ch6_proc_data_16bit[i], \
+                         ch7_raw_data_32bit[i], ch7_proc_data_16bit[i], \
+                         ch8_raw_data_32bit[i], ch8_proc_data_16bit[i], \
+                         ch9_raw_data_32bit[i], ch9_proc_data_16bit[i]])
+
+        rows.append(["AVG:", ch0_raw_avg, ch0_proc_avg, ch1_raw_avg, ch1_proc_avg, ch2_raw_avg, ch2_proc_avg, ch3_raw_avg, ch3_proc_avg, ch4_raw_avg, ch4_proc_avg,\
+           ch5_raw_avg, ch5_proc_avg, ch6_raw_avg, ch6_proc_avg, ch7_raw_avg, ch7_proc_avg, ch8_raw_avg, ch8_proc_avg, ch9_raw_avg, ch9_proc_avg])
+        rows.append(["STD:", ch0_raw_std, ch0_proc_std, ch1_raw_std, ch1_proc_std, ch2_raw_std, ch2_proc_std, ch3_raw_std, ch3_proc_std, ch4_raw_std, ch4_proc_std,\
+           ch5_raw_std, ch5_proc_std, ch6_raw_std, ch6_proc_std, ch7_raw_std, ch7_proc_std, ch8_raw_std, ch8_proc_std, ch9_raw_std, ch9_proc_std])
+
+        for row in rows:
+            wrtr.writerow(row)
+
+    sys.exit(0)
+
 
 print("\n     CH0 32-bit  CH0 16-bit  CH3 32-bit  CH3 16-bit  CH7 32-bit  CH7 16-bit")
 print("     ----------  ----------  ----------  ----------  ----------  ----------")
@@ -303,11 +482,10 @@ print("     ----------  ----------  ----------  ----------  ----------  --------
 print("AVG: %10d  %10d  %10d  %10d  %10d  %10d" % (ch0_raw_avg, ch0_proc_avg, ch3_raw_avg, ch3_proc_avg, ch7_raw_avg, ch7_proc_avg))
 print("STD: %10d  %10d  %10d  %10d  %10d  %10d" % (ch0_raw_std, ch0_proc_std, ch3_raw_std, ch3_proc_std, ch7_raw_std, ch7_proc_std))
 
-row = ["","CH0 32-bit", "CH0 16-bit", "CH3 32-bit", "CH3 16-bit", "CH7 32-bit", "CH7 16-bit"]
-
 with open(outfilename,'wt', newline='') as myfile:
     wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
     rows = []
+    rows.append(["","CH0 32-bit", "CH0 16-bit", "CH3 32-bit", "CH3 16-bit", "CH7 32-bit", "CH7 16-bit"])
     for i in range(10):
         rows.append([i, ch0_raw_data_32bit[i], ch0_proc_data_16bit[i], ch3_raw_data_32bit[i], ch3_proc_data_16bit[i], ch7_raw_data_32bit[i], ch7_proc_data_16bit[i]])
 
