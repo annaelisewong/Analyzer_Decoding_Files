844a76376fee7ef3d6b873ea51fe747b38632791
Anna Elise Wong <awong@tripleringtech.com>
2024-07-09 08:39:16 -0700

    Add timeout for scripts that hang.

--- reg_testing.py
+++ reg_testing.py
@@ -5,6 +5,9 @@ import os
 import csv
 import subprocess
 
+# Variables for use later
+TIMEOUT = 300 # seconds
+
 # Create a variable to make it easier for other people to use the script
 script_path = "C:\\Users\\awong\\Documents\\Analyzer_Decoding_Files\\"
 
@@ -25,168 +28,211 @@ statuswriter = csv.writer(status_tracker, delimiter = ',')
 titles = ['Script Names'] + [os.path.basename(rotor_name) for rotor_name in rotor_names]
 statuswriter.writerow(titles)
 
-# # spi_pho_raw3.py
-# print("\n\n* spi_pho_raw3.py\n")
-# file_count = 0
-# status = ["spi_pho_raw3.py"]
-# for rotor_name in rotor_names:
-#     rotor_name = os.path.abspath(rotor_name)
-#     # Make sure Results folder exists
-#     if not os.path.exists(os.path.dirname(rotor_name).replace("Exports", "Reports")):
-#         os.makedirs(os.path.dirname(rotor_name).replace("Exports", "Reports"))
+# spi_pho_raw3.py
+print("\n\n* spi_pho_raw3.py\n")
+file_count = 0
+status = ["spi_pho_raw3.py"]
+for rotor_name in rotor_names:
+    rotor_name = os.path.abspath(rotor_name)
+    # Make sure Results folder exists
+    if not os.path.exists(os.path.dirname(rotor_name).replace("Exports", "Reports")):
+        os.makedirs(os.path.dirname(rotor_name).replace("Exports", "Reports"))
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "spi_pho_raw3.py", "-r", rotor_name, "-a", "Serial"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "spi_pho_raw3.py", "-r", rotor_name, "-a", "Serial"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# pc104decode3.py
+print("\n* pc104decode3.py\n")
+file_count = 0
+status = ["pc104decode3.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "pc104decode3.py", "-r", rotor_name], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # pc104decode3.py
-# print("\n* pc104decode3.py\n")
-# file_count = 0
-# status = ["pc104decode3.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "pc104decode3.py", "-r", rotor_name], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# Ris_File_Dump.py
+print("\n* Ris_File_Dump.py\n")
+file_count = 0
+status = ["Ris_File_Dump.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "Ris_File_Dump.py", "-r", rotor_name], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # Ris_File_Dump.py
-# print("\n* Ris_File_Dump.py\n")
-# file_count = 0
-# status = ["Ris_File_Dump.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "Ris_File_Dump.py", "-r", rotor_name], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# spi_u18_raw3.py
+print("\n* spi_u18_raw3.py\n")
+file_count = 0
+status = ["spi_u18_raw3.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "spi_u18_raw3.py", "-r", rotor_name, "-a"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # spi_u18_raw3.py
-# print("\n* spi_u18_raw3.py\n")
-# file_count = 0
-# status = ["spi_u18_raw3.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "spi_u18_raw3.py", "-r", rotor_name, "-a"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# motcmd3.py
+print("\n* motcmd3.py\n")
+file_count = 0
+status = ["motcmd3.py"]
+LEGACY = False
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "motcmd3.py", "-r", rotor_name], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if not LEGACY and result.returncode != 0:
+        LEGACY = True
+        # Needs to be rerun with the legacy filename swap consideration
+        try:
+            result = subprocess.run(["python3", script_path + "motcmd3.py", "-r", rotor_name, "-l"], capture_output=False, text=True, timeout=TIMEOUT)
+        except subprocess.TimeoutExpired:
+            status.append("no")
+            continue
+    if result.returncode != 0 and LEGACY:
+        status.append("no")
+    else:
+        status.append("yes")
+    LEGACY = False
+statuswriter.writerow(status)
 
-# # motcmd3.py
-# print("\n* motcmd3.py\n")
-# file_count = 0
-# status = ["motcmd3.py"]
-# LEGACY = False
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "motcmd3.py", "-r", rotor_name], capture_output=False, text=True)
-#     if not LEGACY and result.returncode != 0:
-#         LEGACY = True
-#         # Needs to be rerun with the legacy filename swap consideration
-#         result = subprocess.run(["python3", script_path + "motcmd3.py", "-r", rotor_name, "-l"], capture_output=False, text=True)
-#     if result.returncode != 0 and LEGACY:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-#     LEGACY = False
-# statuswriter.writerow(status)
+# pho_raw_beak.py
+print("\n* pho_raw_beak.py\n")
+file_count = 0
+status = ["pho_raw_beak.py"]
+gcd_fp = "C:\\Users\\awong\\Documents\\Analyzer_Decoding_Files\\analyzer_gcds.txt"
+result = subprocess.run(["python3", script_path + "RunAll_pho_raw_beak.py", "-f", gcd_fp], capture_output=False, text=True)
+# if result.returncode != 0:
+#     status.append("no")
+# else:
+#     status.append("yes")
+statuswriter.writerow(status)
 
-# # pho_raw_beak.py
-# print("\n* pho_raw_beak.py\n")
-# file_count = 0
-# status = ["pho_raw_beak.py"]
-# gcd_fp = "C:\\Users\\awong\\Documents\\Analyzer_Decoding_Files\\analyzer_gcds.txt"
-# result = subprocess.run(["python3", script_path + "RunAll_pho_raw_beak.py", "-f", gcd_fp], capture_output=False, text=True)
-# # if result.returncode != 0:
-# #     status.append("no")
-# # else:
-# #     status.append("yes")
-# statuswriter.writerow(status)
+# extract_beak_timing_stats.py
+print("\n* extract_beak_timing_stats.py\n")
+file_count = 0
+status = ["extract_beak_timing_stats.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "extract_beak_timing_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # extract_beak_timing_stats.py
-# print("\n* extract_beak_timing_stats.py\n")
-# file_count = 0
-# status = ["extract_beak_timing_stats.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "extract_beak_timing_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# extract_beak_offset_stats.py
+print("\n* extract_beak_offset_stats.py\n")
+file_count = 0
+status = ["extract_beak_offset_stats.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "extract_beak_offset_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # extract_beak_offset_stats.py
-# print("\n* extract_beak_offset_stats.py\n")
-# file_count = 0
-# status = ["extract_beak_offset_stats.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "extract_beak_offset_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# extract_hold_time_stats.py
+print("\n* extract_hold_time_stats.py\n")
+file_count = 0
+status = ["extract_hold_time_stats.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "extract_hold_time_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # extract_hold_time_stats.py
-# print("\n* extract_hold_time_stats.py\n")
-# file_count = 0
-# status = ["extract_hold_time_stats.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "extract_hold_time_stats.py", "-r", rotor_name, "-o"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# count_pho_readings_per_beak.py
+print("\n* count_pho_readings_per_beak.py\n")
+file_count = 0
+status = ["count_pho_readings_per_beak.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "count_pho_readings_per_beak.py", "-i", rotor_name, "-o"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
-# # count_pho_readings_per_beak.py
-# print("\n* count_pho_readings_per_beak.py\n")
-# file_count = 0
-# status = ["count_pho_readings_per_beak.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "count_pho_readings_per_beak.py", "-i", rotor_name, "-o"], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
-
-# # extract_phase_timestamps.py
-# print("\n* extract_phase_timestamps.py\n")
-# file_count = 0
-# status = ["extract_phase_timestamps.py"]
-# for rotor_name in rotor_names:
-#     file_count += 1
-#     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-#     result = subprocess.run(["python3", script_path + "extract_phase_timestamps.py", "-r", rotor_name], capture_output=False, text=True)
-#     if result.returncode != 0:
-#         status.append("no")
-#     else:
-#         status.append("yes")
-# statuswriter.writerow(status)
+# extract_phase_timestamps.py
+print("\n* extract_phase_timestamps.py\n")
+file_count = 0
+status = ["extract_phase_timestamps.py"]
+for rotor_name in rotor_names:
+    file_count += 1
+    print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
+    try:
+        result = subprocess.run(["python3", script_path + "extract_phase_timestamps.py", "-r", rotor_name], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
+    if result.returncode != 0:
+        status.append("no")
+    else:
+        status.append("yes")
+statuswriter.writerow(status)
 
 # extract_temps.py
 print("\n* extract_temps.py\n")
@@ -195,7 +241,11 @@ status = ["extract_temps.py"]
 for rotor_name in rotor_names:
     file_count += 1
     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
-    result = subprocess.run(["python3", script_path + "extract_temps.py", "-r", rotor_name, "-o", "-s", "-t"], capture_output=False, text=True)
+    try:
+        result = subprocess.run(["python3", script_path + "extract_temps.py", "-r", rotor_name, "-o", "-s", "-t"], capture_output=False, text=True, timeout=TIMEOUT)
+    except subprocess.TimeoutExpired:
+        status.append("no")
+        continue
     if result.returncode != 0:
         status.append("no")
     else:
@@ -212,3 +262,5 @@ status_tracker.close()
 #     file_count += 1
 #     print("  (%d/%d) Input file prefix: %s" % (file_count, len(rotor_names), rotor_name))
 #     result = subprocess.run(["python3", script_path + "compare_tolerances.py", "-r", rotor_name, "-t", script_path + "Tolerances.csv"], capture_output=False, text=True)
+
+print("DONE")
