2d3e56f93e62a89bed60cd8dfe3530e2654db36a
Anna Elise Wong <awong@tripleringtech.com>
2024-07-09 09:58:26 -0700

    Update the inputs to extract temps file

--- RunAll_extract_temps.py
+++ RunAll_extract_temps.py
@@ -4,10 +4,11 @@ import subprocess
 import getopt
 
 def usage():
-    print("RunAll_extract_temps.py [-o] [-s] [-t]")
+    print("RunAll_extract_temps.py [-o] [-s] [-t] [-d]")
     print(" -o Flag to indicate output file should be created")
     print(" -s Flag to indicate figure should be saved")
     print(" -t Flag to indicate phase timestamp overlay should be shown on plot")
+    print(" -d Flag to indicate that the plot will be displayed to the user.")
     sys.exit(0)
 
 argc = len(sys.argv)
@@ -17,10 +18,11 @@ file_list = [os.path.join(dirpath, filename) for dirpath, _, filenames in os.wal
 CREATE_OUTPUT_FILE = ""
 SAVE_PLOT = ""
 TIMESTAMP_OVERLAY = ""
+DISPLAY_PLOT = ""
 
 if argc > 1:
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ost")
+        opts, args = getopt.getopt(sys.argv[1:], "ostd")
     except getopt.error:
         usage()
         sys.exit(2)
@@ -32,10 +34,12 @@ if argc > 1:
             SAVE_PLOT = "-s"
         elif o == "-t":
             TIMESTAMP_OVERLAY = "-t"
+        elif o == "-d":
+            DISPLAY_PLOT = "-d"
 
 
 for file in file_list:
 
     rotor_name = file.replace("_Group1Data.txt", "")
-    p = subprocess.Popen(["python3", "C:\\Users\\awong\\Documents\\Analyzer_Decoding_Files\\extract_temps.py", "-r", rotor_name, CREATE_OUTPUT_FILE, SAVE_PLOT, TIMESTAMP_OVERLAY])
+    p = subprocess.Popen(["python3", "C:\\Users\\awong\\Documents\\Analyzer_Decoding_Files\\extract_temps.py", "-r", rotor_name, CREATE_OUTPUT_FILE, SAVE_PLOT, TIMESTAMP_OVERLAY, DISPLAY_PLOT])
     p.wait()
